59da28fef508d6d0cbf64cf382cea87d4592ee48
Thoriq As <72277295+Muhthoriqas@users.noreply.github.com>
2023-04-09 03:58:06 +0800

    Update file name

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'devicon/devicon.min.css';
 import 'animate.css';
-import './global.css';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
